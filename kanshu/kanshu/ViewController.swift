be81bd97c1c967c8e9cc63cd47747d34dc432588
Stuart Varrall <github@fpstudios.com>
2015-01-11 21:44:44 +0000

    (new) layout and button actions
    actions bring up alert view

--- kanshu/kanshu/ViewController.swift
+++ kanshu/kanshu/ViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+	@IBOutlet weak var emailField: UITextField!
+	@IBOutlet weak var usernameField: UITextField!
+	@IBOutlet weak var passwordField: UITextField!
+	@IBOutlet weak var readingAbilityField: UITextField!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,38 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	
+	//Alert View
+	func showAlertView(title: String) {
+	
+		let alertView = UIAlertController(title: title, message: "\(title) Button Pressed", preferredStyle: UIAlertControllerStyle.Alert)
+		
+		let cancelAction: UIAlertAction = UIAlertAction(title: "Dismiss", style: .Cancel) { action -> Void in
+			//Just dismiss the action sheet
+		}
+		
+		alertView.addAction(cancelAction)
+		
+		self.presentViewController(alertView, animated: true, completion: nil)
+		
+	}
+	
+	//Button Actions
+	@IBAction func signinAction(sender: UIButton) {
+		self.showAlertView("Sign In")
+	}
 
+	@IBAction func signupAction(sender: UIButton) {
+		self.showAlertView("Sign Up")
+	}
+
+	@IBAction func skipAction(sender: UIButton) {
+		self.showAlertView("Skip")
+	}
+	
+	//Text Field Delegates
+	func textFieldDidBeginEditing(textField: UITextField) {
+
+	}
 }
 
